6ffc2d9c34891b5769e9c63cb56eaea09d0c6f90
Marco Antonio <marco.nunes@hfa.mil.br>
2025-01-14 09:03:05 -0300

    .

--- resources/js/Components/SobrePerfil.vue
+++ resources/js/Components/SobrePerfil.vue
@@ -1,8 +1,37 @@
 <template>
     <div class="px-6 py-2">
-        <div class="px-16 mb-16 2xl:divide-x grid 2xl:grid-cols-2 max-[1536px]:divide-y gap-5 cor-texto74">
-            <div class="">"Escritor de fantasia e ficção científica. Sonhador apaixonado por criar mundos."</div>
-            <div class="text-end bitter-500-italic text-base">"Escritor de fantasia e ficção científica. Sonhador apaixonado por criar mundos."</div>
+        <div class="px-10 mb-16 2xl:divide-x grid 2xl:grid-cols-2 max-[1536px]:divide-y gap-5 cor-texto74 items-center py-3">
+            <div class="grid gap-5">
+
+                <div class="bitter-500-italic text-base">
+                    <span>"Escritor de fantasia e ficção científica. Sonhador apaixonado por criar mundos."</span>
+                </div>
+                <div class="flex justify-center gap-3 py-5">
+                    <div class="font-semibold bg-fundo-pagina grid justify-items-center p-3 rounded-md text-xl">
+                        <span class="text-orange-500">10</span>
+                        Obras Escritas
+                    </div>
+                    <div class="font-semibold bg-fundo-pagina grid justify-items-center p-3 rounded-md text-xl">
+                        <span class="text-orange-500">10</span>
+                        Obras Lidas
+                    </div>
+                    <div class="font-semibold bg-fundo-pagina grid justify-items-center p-3 rounded-md text-xl">
+                        <span class="text-orange-500">10</span>
+                        Reações
+                    </div>
+                </div>
+            </div>
+            <div class="">
+
+                <div class="flex justify-end gap-5 text-lg py-5 font-semibold">
+
+                </div>
+
+                <div class="text-end">
+                    <span class="text-sm font-semibold">Ingressou em: <span class="text-orange-500">Janeiro de 2025</span>  </span>
+                </div>
+
+            </div>
         </div>
     </div>
 </template>
